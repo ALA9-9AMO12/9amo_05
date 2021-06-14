4c5d7f3bb24e3b12412a8b4de9f536c873104c20
odayy97 <66997254+odayy97@users.noreply.github.com>
2021-06-14 11:19:16 +0200

    Homepage merge layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,9 @@
-<h1>Hoempage</h1>
+@extends('layout')
+
+@section('content')
+    <h1>Homepage</h1>
+@endsection
+@section('footer')
+    <h1>Footer</h1>
+@endsection
+
--- resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('content')
-    <h1>Homepage</h1>
-@endsection
-@section('footer')
-    <h1>Footer</h1>
-@endsection
