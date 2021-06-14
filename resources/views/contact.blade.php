48a95d3b8b0ad1a97169684392f6e2b1a8b37ff8
odayy97 <66997254+odayy97@users.noreply.github.com>
2021-06-14 13:05:06 +0200

    Contact
    Form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,17 @@
 
 @section('content')
     <h1>Contact</h1>
+    <div style="text-align: center; background-color: white;">
+        <form>
+            <label>Name:</label>
+            <input type="text" name="name"><br>
+            <label>Email:</label>
+            <input type="email" name="email"><br>
+            <label>Massage:</label>
+            <input type="text" name="description"><br>
+            <input type="submit" value="Send">
+        </form>
+    </div>
 @endsection
 @section('footer')
     <h1>Footer</h1>
