b3ca54b57464072620982a3f3d0a2951ad3838d7
olivieruijtenhoorn <6004365@mborijnland.nl>
2021-06-15 12:45:37 +0200

    verbeteren van de artikel pagina

--- resources/views/artikellen.blade.php
+++ resources/views/artikellen.blade.php
@@ -1,1 +1,162 @@
-<h1> oli </h1>
+@extends('layout')
+
+@section('content')
+    <h1>Artikelen</h1>
+
+    <div style= "display: flex; flex-direction:column; width: 40vw; align-content: center; float: left;margin-left: 5vw">
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">GEZELLIGE DAG OP OUD POELGEEST</h2>
+            Zondag 25 april was het gezellig druk op afstand op Oud Poelgeest. De vereniging sloot
+            aan op de activiteiten van Oud Poelgeest zelf met het maken van raampjes voor de cursisten
+            en mensen veel te vertellen over de ramen, kasten en bijen zowel tussen de mensen als bij de
+            bijenstand. En in de middag brak ook nog de zon door extra gezellig dus.
+        </div>
+
+
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">NIEUWE PENNINGSMEESTER</h2>
+            Op de ALV van 30 september is afscheid genomen van onze penningmeester Jan Persson.
+            Karel Diephuis is verkozen als zijn opvolger.
+            Karel is een beginnend imker en sinds 2 jaar lid van onze vereniging.
+            Als 65plusser heeft hij tijd en motivatie om binnen het bestuur een bijdrage te leveren aan het financiële reilen
+            en zeilen. In zijn functie van directeur van een grote jeugdzorgorganisatie deed hij de nodige ervaring
+            op met financieel beheer.
+
+        </div>
+
+
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">CORONAVIRUS</h2>
+            Het voorjaar staat op uitbarsten. Als imker hebben staat ons de komende maanden het nodige te doen.
+            De overheidsmaatregelen om het Coronavirus onder de duim te houden, hebben ook effect op het imkeren.
+            Wat ons te wachten staat, is moeilijk in te schatten. De NBV probeert u zo goed mogelijk te informeren
+            over beperkingen en advies te geven wat de mogelijkheden zijn. De nodige berichten zijn op een speciale
+            overzichtspagina op de NBV website samengevat.
+            Vandaag twee nieuwe berichten:
+            Advies over reizen met de volken voor bestuiving van de fruitbloesem bijgekomen. Ook aandacht voor
+            reizen met bijenvolken over de grens naar België of Duitsland.
+            Oproep voor inzameling van propolis om dat samen met honing in te zetten voor patiënten ter verlichting
+            van griepachtige klachten die mogelijk veroorzaakt worden door Corona.
+
+
+        </div>
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">KASTEN OP LANDGOED RHIJNWEIDE</h2>
+            Sinds 26 september heeft de vereniging een nieuwe plek voor kasten.
+            Dank zij een vasthoudende bewoner mag de vereniging kasten plaatsen op Landgoed Rhijnweide.
+            Sjoerd de Boer een van de bewoners van de nieuwe wijk Duijfrak, wilde zijn medebewoners
+            enthousiast maken om een bijenkast te adopteren in de wijk. Toen dat op niets uitliep is
+            hij gaan praten met een wethouder over mogelijkheden van bijen in de wijk. Zo kwam hij in
+            contact met de beheerders van het landgoed Rhijnweide aan de Valkenburgerweg. Daar kreeg hij wel gehoor.
+            Hij had al eerder contact met het bestuur gezocht, en nu was het een serieuze optie voor ons. Een paar
+            mensen van de vereniging, waaronder Hans v.d Post zijn toen het gebied gaan bekijken op zoek naar een
+            geschikte plek voor kasten. Die was snel gevonden, beschut en zonnig. We zijn daarna in gesprek gegaan
+            met de beheerders en hebben afspraken gemaakt. In eerste instantie gaat het om ruimte voor 5 kasten. De
+            eigenaresse is enthousiast en voor haar is een potje honing genoeg in ruil voor de plek. Ons aanvankelijke
+            plan was om in het voorjaar te beginnen. Echter, onverwacht kregen we 2 kasten aangeboden die vrij accuut
+            bij de imker weg moesten omdat hij met imkeren stopte. In overleg met de beheerder hebben Fred Schalker en
+            Sjoerd de Boer de plek gereed gemaakt en afgelopen donderdag zijn de kasten geplaatst. Fred heeft de kasten
+            onder zijn hoede genomen.
+            Mocht iemand een plek nodig hebben en /of interesse hebben in deze plek, dan eerst contact opnemen
+            met Fred of met mij.
+            Hartelijke groet Alma vd Wijngaard
+        </div>
+
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">WESPEN</h2>
+            Meestal zijn wespen niet agressiever dan bijen en hommels. Een groot verschil tussen bijen, hommels
+            en wespen is dat bijen en hommels op zoek zij naar nectar en stuifmeel, terwijl wespen op zoetstoffen
+            afkomen. Wespen dringen dan ook huizen binnen, dat doet bijen en hommels meestal niet. Omdat zoetstoffen
+            in de buurt van mensen zijn komen wespen daarop af. Dit is de redenen waarom wespennesten vaak dichtbij
+            mensen worden aangetroffen, bijvoorbeeld in de tuin, schuurtjes, en dergelijken. Het is aangeraden om niet
+            in de buurt van een wespennest te komen.
+            Zit er een wespennest te dichtbij kunt u het beste contact opnemen met een ongedierte bestrijder.
+        </div>
+    </div>
+
+
+    <div style= "display: flex; width:40vw; flex-direction:column;  align-content: center">
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center; display: block">VANDALISME OP RIJNWEYDE</h2>
+            Helaas zijn er op de bijenstand Rijnweyde een aantal bijenkasten omgetrapt in het weekend an 23-25 april.
+            De leden van de vereniging hebben alles in het werk gesteld om de schade zo goed mogelijk te herstellen
+            en er zijn wat passende maatregelen genomen. Voor tips wordt u verzocht
+            om contact met de politie op te nemen via 0900-8844, ook namens de bijen van harte dank alvast!
+        </div>
+
+
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">ONDERSTEUNING NBV TIJDENS DE CORONA CRISIS</h2>
+            De NBV krijgt momenteel dagelijks vragen over de gevolgen van de Coronacrisis.
+            Leden worden geconfronteerd met praktische problemen,
+            maar hebben ook vragen en/of zorgen over de (nabije) toekomst.
+            Juist in deze onzekere tijd wil de NBV er zijn voor haar leden. Maar hoe kunnen we daarvoor zorgen? Hoe kunnen
+            we jou als lid in deze crisistijd ondersteunen? Welke informatie heb jij nodig van de NBV?
+            Welke zorgen heb je voor nu of de nabije toekomst?
+            Laat je het ons weten? Klik op onderstaande link en vul de beknopte vragenlijst in. Het kan anoniem, maar indien
+            gewenst kan je ook je gegevens invullen.
+
+            https://fd10.formdesk.com/NBV/NBVincoronatijd
+        </div>
+
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">CONTINUITEIT BIJENHOUDERIJ TEN TIJDE VAN CORONA-VIRUS</h2>
+            Hierbij treft u een berichtje aan met een link naar de
+            brief die de Nederlandse imkerbonden vanmorgen hebben gestuurd naar
+            het Ministerie van Landbouw, Natuur en Voedselkwaliteit.
+            De maatregelen ten gevolge van het Corona-virus in Nederland volgen zich op.
+            Ook voor de imkerij heeft dit op dit moment
+            al gevolgen. Indien besloten wordt tot een landelijke lockdown (light),
+            zullen imkers expliciet toestemming moeten krijgen om te reizen, ten gunste
+            van het verzorgen en verplaatsten van hun volken en de zorg rond bijenzwermen.
+            Immers, het hoogseizoen voor de imkerij is aanstaande.
+            De Nederlandse imkerbonden informeerden vanochtend middels een brief het Ministerie van Landbouw,
+            Natuur en Voedselkwaliteit over de noodzakelijke reisbewegingen van imkers, om de Nederlandse imkerij
+            de komende periode te kunnen blijven uitvoeren.
+
+            Wij informeren u natuurlijk over het vervolg.
+
+            Bert Berghoef,
+
+            voorzitter Nederlandse Bijenhoudersvereniging
+
+        </div>
+
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">Varroa Sensitive Hygiene</h2>
+            In het najaar van 2018 in het MEC-gebouw heeft Bart               Barten voor zo’n 25 leden van imkervereniging                Oegstgeest een zeer onderhoudende lezing verzorgd over de bestrijding            Van de mijt (Varroa Destructor).
+            Bart Barten en Tieme Wanders zijn al een aantal jaren bezig met het kweken van “superbijen”. Bart en Tieme zijn                betrokken bij de Stichting Arista Bee Research. Deze stichting legt contacten en bewerkstelligt de samenwerking                tussen professionals, wetenschappers en betrokken vrijwilligers.
+            Wat nu als je simpelweg met elke vorm van varroabestrijding stopt? Deze selectie methode, vrij naar James Bond “Live           and let die” genoemd, is toegepast op het Zweedse eiland Gotland. Na drie jaar waren 129 van de 150 bijenvolken op             het eiland Gotland gedood door varroamijten.
+            Bart vertelde over de inspanningen van BartJan Fernhout die op Hawai een bij heeft ontwikkeld die zelf afrekent met            de Varroa-mijt. Ook legde Bart uit dat in Amerika werd gewerkt aan de resistente bij. Ook de inspanningen van                  Buckfast-telers op Marken met raszuivere aanparingen zijn benoemd.
+            In Nederland wordt onder andere door Bart Barten gewerkt aan het Varroa Sensitive Hygiene programma. Door selectie             op hygiëne kunnen bijen wellicht varroaresistent worden. Het gaat om bijen, die het door varroamijten geïnfecteerd             broed, herkennen en opruimen. Vlak voor de cel van een bijenlarf wordt gesloten “kruipt” een varroa bij de larve.              Bijen kunnen dat ruiken en verwijderen de pop met varroa. Dit laatste is dus de ontdekking dat bijen door de                   gesloten broedcel de varroa kunnen “ruiken”. Door elk jaar van het beste volk – geselecteerd uit 40 volken (mini               plus kastjes)  koninginnen te kweken waarvan de bijen steeds meer varroa’s vinden zal je volken krijgen die niet               meer een “varroa bestrijding” nodig hebben.
+            Bart liet aan de aan van foto’s zien wat er zo al bij komt kijken. Het gaat om de selectie van koninginnen maar ook            om darren. En dat niet alleen; ook het kweken van mijten moest hij onder de knie krijgen. Poedersuiker blijkt                  daarbij onmisbaar.
+            Bij wijze van spreken tegen wil en dank zullen we, Oegstgeester imkers, voorlopig nog “gedwongen” met gif                      (mierenzuur/ oxaal zuur) aan de slag moeten. Maar er gloort hoop!
+            Kijk ook eens op: aristabeeresearch.org en uitzendinggemist.net
+            Bartjan Fernhout  mei 2018 – VSH
+        </div>
+
+
+        <div style="width: 30vw; margin-top: 15px;background-color: darkgrey; opacity: 0.6; border-radius:
+        20px;padding-left: 20px;padding-bottom: 20px; padding-right: 20px">
+            <h2 style="text-align: center;">Het maken van een kunstzwerm</h2>
+            Ieder bijenvolk wil zich vermeerderen, dit doen zij door het volk te delen, waarbij de koningin (moer) met een groot           gedeelte van het volk besluit om te vertrekken, dit noemen ze zwermen. Je kan er van uit gaan dat dit niet gebeurt             op het tijdstip van jouw keuze. De enige manier voor de imker, om deze vermeerdering op zijn of haar voorwaarden               te laten plaatsvinden, is om op een min of meer kunstmatige wijze, het volk te splitsen. Ook wel genoemd, het maken            van een koninginnen aflegger of kunstzwerm.
+            Ter voorbereiding van deze operatie heb je een extra kast(je), met zes of tien ramen. In dit kastje heb je ramen met           kunstraat en minimaal een raam met verzegeld voer vanuit het hoofdvolk, de vliegspleet van de nieuwe kast moet                 gesloten zijn! Onderstaand de volgorde van werken, indien er een combinatie is van twee broedkamers en een of                  meerdere  honingkamers.
+        </div>
+    </div>
+@endsection
+
+
+@section('footer')
+    <h1 style="margin-top: 10vw">Footer</h1>
+@endsection
+
+
