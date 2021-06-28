738143b8985564e2683eba4263a244b0edbac16f
hasanmarrawi <81229498+hasanmaarawi@users.noreply.github.com>
2021-06-28 14:49:55 +0200

    basiscursus view controller route gemaakt

---
+++ app/Http/Controllers/BasiscursusController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BasiscursusController extends Controller
+{
+    public function index(){
+        view('basiscursus');
+    }
+}
