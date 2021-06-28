7418213559199e1acd194c2b22e784a61400aa55
hasanmarrawi <81229498+hasanmaarawi@users.noreply.github.com>
2021-06-28 14:29:31 +0200

    cursus view en controller en route gemaakt

---
+++ app/Http/Controllers/CursusblogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursusblogController extends Controller
+{
+    public function index()
+    {
+        return view('cursusblog');
+    }
+    }
